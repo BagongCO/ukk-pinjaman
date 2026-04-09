7e55e16fbb927d72f73f046923fd6419f02b8a55
chriss <rizkycharisda@gmail.com>
2026-04-09 09:00:46 +0700

    y

--- backend/pages/data_peminjaman/proses/proses_tambah.php
+++ backend/pages/data_peminjaman/proses/proses_tambah.php
@@ -2,9 +2,21 @@
 session_start();
 include "../../../app.php";
 
-$id = $_GET['id'];
+// Perbaiki: Gunakan 'id_barang' bukan 'id'
+$id_barang = isset($_GET['id_barang']) ? $_GET['id_barang'] : (isset($_GET['id']) ? $_GET['id'] : null);
 
-$data = mysqli_query($connect, "SELECT * FROM barang WHERE id_barang='$id'");
+if (!$id_barang) {
+    echo "ID barang tidak ditemukan!";
+    exit;
+}
+
+$data = mysqli_query($connect, "SELECT * FROM barang WHERE id_barang='$id_barang'");
+
+if (!$data || mysqli_num_rows($data) == 0) {
+    echo "Barang tidak ditemukan!";
+    exit;
+}
+
 $row = mysqli_fetch_assoc($data);
 ?>
 
@@ -12,76 +24,71 @@ $row = mysqli_fetch_assoc($data);
 <html>
 
 <head>
-
-    <title><?php echo $row['nama_barang']; ?></title>
-
+    <title><?php echo htmlspecialchars($row['nama_barang']); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
 </head>
 
 <body>
 
-    <?php include "partials/navbar.php"; ?>
+    <?php 
+    // Perbaiki path include
+    $navbar_path = __DIR__ . "/../../../partials/navbar.php";
+    if (file_exists($navbar_path)) {
+        include $navbar_path;
+    } else {
+        echo '<div class="alert alert-warning">Navbar tidak ditemukan</div>';
+    }
+    ?>
 
     <div class="container mt-5">
-
         <div class="row">
-
             <div class="col-md-6">
-
-                <img src="../storage/barang/<?php echo $row['foto']; ?>" class="img-fluid">
-
+                <?php if (!empty($row['foto']) && file_exists("../storage/barang/" . $row['foto'])): ?>
+                    <img src="../storage/barang/<?php echo htmlspecialchars($row['foto']); ?>" class="img-fluid">
+                <?php else: ?>
+                    <img src="https://via.placeholder.com/400" class="img-fluid" alt="No Image">
+                <?php endif; ?>
             </div>
 
             <div class="col-md-6">
-
-                <h2><?php echo $row['nama_barang']; ?></h2>
-
-                <p><?php echo $row['deskripsi']; ?></p>
-
+                <h2><?php echo htmlspecialchars($row['nama_barang']); ?></h2>
+                <p><?php echo htmlspecialchars($row['deskripsi']); ?></p>
+                
                 <h4>
-                    Rp <?php echo number_format($row['harga_per_jam']); ?> / jam
+                    Rp <?php echo number_format($row['harga_per_jam'] ?? 0); ?> / jam
                 </h4>
 
-                <?php if (isset($_SESSION['id_user'])) { ?>
-
-                    <!-- FORM PINJAM -->
-
+                <?php if (isset($_SESSION['id_user'])): ?>
                     <form action="proses_pinjam.php" method="POST">
-
                         <input type="hidden" name="id_barang" value="<?php echo $row['id_barang']; ?>">
                         <input type="hidden" name="id_user" value="<?php echo $_SESSION['id_user']; ?>">
 
                         <div class="mt-3">
-
                             <label>Durasi Sewa (Jam)</label>
-
                             <input type="number" name="durasi_jam" class="form-control" required min="1">
-
                         </div>
 
                         <button class="btn btn-success mt-3">
                             Sewa Sekarang
                         </button>
-
                     </form>
-
-                <?php } else { ?>
-
-                    <a href="auth/login.php" class="btn btn-primary mt-3">
+                <?php else: ?>
+                    <a href="../../auth/login.php" class="btn btn-primary mt-3">
                         Login untuk menyewa
                     </a>
-
-                <?php } ?>
-
+                <?php endif; ?>
             </div>
-
         </div>
-
     </div>
 
-    <?php include "partials/footer.php"; ?>
+    <?php 
+    $footer_path = __DIR__ . "/../../../partials/footer.php";
+    if (file_exists($footer_path)) {
+        include $footer_path;
+    }
+    ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
